d8011a500ec29e1f7a7ed17680145e4cec835712
1078788262 <1078788262@qq.com>
2026-07-08 14:27:18 +0800

    first commit

---
+++ src/main/java/com/example/corelearning/controller/userController.java
@@ -0,0 +1,4 @@
+package com.example.corelearning.controller;
+
+public class userController {
+}
---
+++ src/test/java/com/example/corelearning/CoreLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.corelearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
