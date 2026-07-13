b538371c0f4e8887b7490e331b481699f112d752
07XYGIN <XYGIN0708@163.COM>
2026-07-13 16:28:37 +0800

    fix Reset Project

--- spring-boot-basic/src/main/java/com/example/corelearning/mapper/UserMapper.java
+++ spring-boot-basic/src/main/java/com/example/corelearning/mapper/UserMapper.java
@@ -1,17 +1,9 @@
 package com.example.corelearning.mapper;
 
-import com.example.corelearning.dto.UserDto;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
-import java.util.List;
 
 @Mapper
 public interface UserMapper {
-    List<UserDto> selectByCondition(UserDto condition);
-    int decreaseBalance(@Param("id") Integer id, @Param("amount") Integer amount);
-    int increaseBalance(@Param("id") Integer id, @Param("amount") Integer amount);
-    int insertUser(@Param("username") String username, @Param("password") String password);
-    UserDto getUserInfo(@Param("username") String username);
-    UserDto selectUserWithOrders(@Param("id") Integer id);
+
 }
